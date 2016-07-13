226e3ad7e6a23f0d045a27be0fe3e929ec25edef
Duncan-Liang <326219589@qq.com>
2016-07-13 20:05:43 +0800

    Readme update

--- demo/silenceuninstall/app/src/main/java/silenceuninstall/test/com/silenceuninstall/MainActivity.java
+++ demo/silenceuninstall/app/src/main/java/silenceuninstall/test/com/silenceuninstall/MainActivity.java
@@ -1,6 +1,9 @@
 package silenceuninstall.test.com.silenceuninstall;
 
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInstaller;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +14,7 @@ import silenceuninstall.test.com.runtime.RunScript;
 
 public class MainActivity extends AppCompatActivity {
     private Button slienceUninstall;
+    private Button packageInstall;
     private Context mContext;
 
     @Override
@@ -22,9 +26,28 @@ public class MainActivity extends AppCompatActivity {
         slienceUninstall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String result = RunScript.runIt("pm uninstall -k com.ss.android.article.news");
+                String result = RunScript.runIt("pm uninstall -k uninstalltest.test.com.uninstalltest");
                 Toast.makeText(mContext,result,Toast.LENGTH_LONG);
             }
         });
+
+
+        packageInstall = (Button)findViewById(R.id.packageInstall);
+        packageInstall.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                runUninstall();
+            }
+        });
     }
+
+    private void runUninstall()
+    {
+        String appPackage = "uninstalltest.test.com.uninstalltest";
+        Intent intent = new Intent(mContext, mContext.getClass());
+        PendingIntent sender = PendingIntent.getActivity(mContext, 0, intent, 0);
+        PackageInstaller mPackageInstaller = mContext.getPackageManager().getPackageInstaller();
+        mPackageInstaller.uninstall(appPackage, sender.getIntentSender());
+    }
+
 }
